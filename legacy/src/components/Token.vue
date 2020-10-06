54793acfadf0c8f0ba0a105536f756c414223450
WalaNour <nourwala548@gmail.com>
2020-10-06 14:15:51 +0100

    just a schema

---
+++ legacy/src/components/Token.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    <img src="../assets/token.jpg" alt="" id="tokenImage"/>
+    <h3 >MJ</h3>
+    </div>
+</template>
+<script>
+export default {
+    name:"Token"
+}
+</script>
