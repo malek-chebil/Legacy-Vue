aaab93c631a1ff7cd90a6eb484ecf68893e8f3cf
WalaNour <nourwala548@gmail.com>
2020-10-06 15:40:18 +0100

    Shop is done

---
+++ legacy/src/components/ShopAvatar/Avatar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <img >
+    <h1></h1>
+    <h2></h2>
+    <button>Puchase</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Avatar',
+}
+</script>
