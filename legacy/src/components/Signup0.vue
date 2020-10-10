19bbd7cfa7f166d0f6249631acc5bde9c4669ca0
malek-chebil <chebil1203@gmail.com>
2020-10-10 12:21:39 +0100

    signUp0 done

--- legacy/src/components/Signup0.vue
+++ legacy/src/components/Signup0.vue
@@ -47,8 +47,8 @@ export default {
         id: this.id,
         currentskin: this.i,
       };
-      axios.post("/selectChar", data).then((data) => {
-        console.log('slim : 4 data sent to be saved=>', data)
+      axios.post("/selectChar", data).then(() => {
+        // console.log('slim : 4 data sent to be saved=>', data)
         this.$emit("Mlogin");
       })
       .catch(err => console.log(err))
@@ -58,6 +58,53 @@ export default {
 </script>
 
 
-<style scoped>
+<style>
+
+.body {
+    position: relative;
+    top: 29px;
+    left: 450px;
+    background: #f6f5f7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: "Montserrat", sans-serif;
+    height: 65vh;
+    width: 400px;
+    border: #592bff;
+    border-radius: 10%;
+    border-style: solid;
+  }
+   #Selectchar{
+    position: relative;
+    top:-55px;
+  }
+  #CardPlayer{
+    position: relative;
+    top:-40px;
+    border: solid black 1PX;
+    border-radius: 8px;
+  }
+  #arrowR{
+    position: relative;
+    left:100px;
+    transform: rotate(270deg);
+    width:40px;
+    top: 50px;
+  }
+  #arrowL{
+    position: relative;
+    left: -101px;
+    transform: rotate(90deg);
+    width:40px;
+    top: -139px;
+  }
+ #BTNS{
+   position: relative;
+   top:-20px;
+ }
+
 </style>
 
+  
